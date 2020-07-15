6ad3895e4a599637e47c249ab469da13837dc22d
Matthias Heinzel <mheinzel@posteo.de>
2020-07-15 19:59:13 +0200

    implement create_game

--- bot/games/elon_musk/core/game.py
+++ bot/games/elon_musk/core/game.py
@@ -1,2 +1,18 @@
+import bot.games.elon_musk.core.message as message
+
 class Game:
-    pass
+    def __init__(self, chat, code):
+        self.chat = chat
+        self.code = code
+        self.participants = []
+        self.current_round = None
+        self.history = []
+
+    def init(self):
+        created = message.GameCreated(self.chat, self.code)
+        return [created]
+
+def create_game(chat, code):
+    game = Game(chat, code)
+    responses = game.init()
+    return game, responses
