409d9c603402ba2b3e4a5b3781892facfaddf611
Matthias Heinzel <mheinzel@posteo.de>
2020-07-15 19:55:34 +0200

    fix tests: isinstance

--- tests/games/elon_musk/test_core.py
+++ tests/games/elon_musk/test_core.py
@@ -19,7 +19,7 @@ group1 = Chat(101, 'group')
 # create new game
 game1, res = create_game(chat = group1, code = "ELON123")
 assert len(res) == 1
-assert res[0] is msg.GameCreated
+assert isinstance(res[0], msg.GameCreated)
 assert res[0].chat == group1
 assert res[0].code == "ELON123"
 assert game1.participants == []
@@ -30,28 +30,28 @@ assert game1.history == []
 # user1
 res = game1.join_private(user = user1, chat = private1)
 assert len(res) == 2
-assert res[0] is msg.GameJoinedPrivate
+assert isinstance(res[0], msg.GameJoinedPrivate)
 assert res[0].chat == private1
 assert res[0].joined_user == user1
-assert res[1] is msg.GameJoined
+assert isinstance(res[1], msg.GameJoined)
 assert res[1].chat == group1
 assert res[1].joined_user == user1
 # user2
 res = game1.join_private(user = user2, chat = private2)
 assert len(res) == 2
-assert res[0] is msg.GameJoinedPrivate
+assert isinstance(res[0], msg.GameJoinedPrivate)
 assert res[0].chat == private2
 assert res[0].joined_user == user2
-assert res[1] is msg.GameJoined
+assert isinstance(res[1], msg.GameJoined)
 assert res[1].chat == group1
 assert res[1].joined_user == user2
 # user3
 res = game1.join_private(user = user3, chat = private3)
 assert len(res) == 2
-assert res[0] is msg.GameJoinedPrivate
+assert isinstance(res[0], msg.GameJoinedPrivate)
 assert res[0].chat == private3
 assert res[0].joined_user == user3
-assert res[1] is msg.GameJoined
+assert isinstance(res[1], msg.GameJoined)
 assert res[1].chat == group1
 assert res[1].joined_user == user3
 # some checks
@@ -63,7 +63,7 @@ assert game1.private_chats[user3.id] == private3
 # at least 4 participants are needed to start a game
 res = game1.start_round()
 assert len(res) == 1
-assert res[0] is msg.GameNotEnoughParticipants
+assert isinstance(res[0], msg.GameNotEnoughParticipants)
 assert res[0].chat == group1
 assert res[0].participants == [user1, user2, user3]
 assert game1.current_round is None
@@ -71,10 +71,10 @@ assert game1.current_round is None
 # user4
 res = game1.join_private(user = user3, chat = private3)
 assert len(res) == 2
-assert res[0] is msg.GameJoinedPrivate
+assert isinstance(res[0], msg.GameJoinedPrivate)
 assert res[0].chat == private3
 assert res[0].joined_user == user3
-assert res[1] is msg.GameJoined
+assert isinstance(res[1], msg.GameJoined)
 assert res[1].chat == group1
 assert res[1].joined_user == user3
 
@@ -82,15 +82,15 @@ assert res[1].joined_user == user3
 res = game1.start_round()
 assert len(res) == 2
 # new game is announced
-assert res[0] is msg.RoundStarted
+assert isinstance(res[0], msg.RoundStarted)
 assert res[0].chat == group1
 assert res[0].participants == [user1, user2, user3, user4]
 assert res[0].giving_problem == user1
 # one person is asked to describe a problem (privately)
-assert res[1] is msg.RoundDemandProblem
+assert isinstance(res[1], msg.RoundDemandProblem)
 assert res[1].chat == private1
 assert res[1].user == user1
-assert res[1].reply_context is rpl.SubmitProblem
+assert isinstance(res[1].reply_context, rpl.SubmitProblem)
 round1 = res[1].reply_context.round
 assert game1.current_round.id == round1
 assert game1.current_round.giving_problem == user1.id
@@ -101,22 +101,22 @@ problem1 = Problem(user1, "Life is too short.")
 res = game1.submit_problem(round1, problem1)
 assert len(res) == 2
 # acknowledge
-assert res[0] is msg.RoundAcceptProblem
+assert isinstance(res[0], msg.RoundAcceptProblem)
 assert res[0].chat == private1
 # update the group chat
-assert res[1] is msg.RoundDemandSolutions
+assert isinstance(res[1], msg.RoundDemandSolutions)
 assert res[1].chat == group1
 assert res[1].problem == problem1
 assert res[1].giving_problem == user1
 assert res[1].solution_order == [user2, user3, user4]
-assert res[1].reply_context is rpl.SubmitSolution
+assert isinstance(res[1].reply_context, rpl.SubmitSolution)
 assert res[1].reply_context.round == round1
 # tell every user the problem (first one is the randomly chosen "Elon Musk")
-assert res[2] is msg.RoundNotifyProblem
+assert isinstance(res[2], msg.RoundNotifyProblem)
 assert res[2].problem == Problem(user1, "Elon Musk")
-assert res[3] is msg.RoundNotifyProblem
+assert isinstance(res[3], msg.RoundNotifyProblem)
 assert res[3].problem == problem1
-assert res[4] is msg.RoundNotifyProblem
+assert isinstance(res[4], msg.RoundNotifyProblem)
 assert res[4].problem == problem1
 assert set(r.chat for r in res[2:4]) == {private2, private3, private4}
 round1_elon = res[2].user
@@ -134,21 +134,21 @@ assert len(res) == 0 # TODO any further updates here?
 # solutions can only be submitted once
 res = game1.submit_solution(round1, solution_user2)
 assert len(res) == 1
-assert res[0] is msg.SolutionAlreadySubmitted
+assert isinstance(res[0], msg.SolutionAlreadySubmitted)
 assert res[0].chat == group1
 assert res[0].solution == solution_user2
 
 # user4 is still missing, so we can't reveal
 res = game1.reveal()
 assert len(res) == 1
-assert res[0] is msg.RoundNotFinishedYet
+assert isinstance(res[0], msg.RoundNotFinishedYet)
 assert res[0].chat == group1
 assert res[0].missing_solutions == [user4]
 
 # user4 submits solution
 res = game1.submit_solution(round1, solution_user4)
 assert len(res) == 1
-assert res[0] is msg.RoundSummary
+assert isinstance(res[0], msg.RoundSummary)
 assert res[0].chat == group1
 assert res[0].problem == problem1
 assert len(res[0].solutions) == 3
@@ -159,7 +159,7 @@ assert res[0].solutions[2] == solution_user4
 # the game is revealed
 res = game1.reveal()
 assert len(res) == 1
-assert res[0] is msg.RoundRevealed
+assert isinstance(res[0], msg.RoundRevealed)
 assert res[0].chat == group1
 assert res[0].elon_musk == round1_elon
 
@@ -168,15 +168,15 @@ assert res[0].elon_musk == round1_elon
 res = game1.start_round()
 assert len(res) == 2
 # new game is announced
-assert res[0] is msg.RoundStarted
+assert isinstance(res[0], msg.RoundStarted)
 assert res[0].chat == group1
 assert res[0].participants == [user1, user2, user3, user4, user5]
 assert res[0].giving_problem == user2
 # one person is asked to describe a problem (privately)
-assert res[1] is msg.RoundDemandProblem
+assert isinstance(res[1], msg.RoundDemandProblem)
 assert res[1].chat == private2
 assert res[1].user == user2
-assert res[1].reply_context is rpl.SubmitProblem
+assert isinstance(res[1].reply_context, rpl.SubmitProblem)
 round2 = res[1].reply_context.round
 assert game1.current_round.id == round2
 assert game1.current_round.giving_problem == user2.id
