8b20dff314594504ee15b6e12247cf1310a17abc
Matthias Heinzel <mheinzel@posteo.de>
2020-07-19 13:14:14 +0200

    add Message superclass for all messages

--- bot/games/elon_musk/core/message.py
+++ bot/games/elon_musk/core/message.py
@@ -3,59 +3,64 @@ import bot.games.elon_musk.core.reply as reply
 from bot.games.elon_musk.core.shared import Problem, Solution
 from telegram import Chat, User
 
-class GameCreated(NamedTuple):
+# I would like to move the 'chat' attribute here, but subclass relationships
+# between NamedTuples don't seem to work as expected.
+class Message:
+    pass
+
+class GameCreated(Message, NamedTuple):
     chat: Chat
     code: str
 
-class GameJoinedPrivate(NamedTuple):
+class GameJoinedPrivate(Message, NamedTuple):
     chat: Chat
     joined_user: User
 
-class GameJoined(NamedTuple):
+class GameJoined(Message, NamedTuple):
     chat: Chat
     joined_user: User
 
-class GameNotEnoughParticipants(NamedTuple):
+class GameNotEnoughParticipants(Message, NamedTuple):
     chat: Chat
     participants: List[User]
 
-class RoundStarted(NamedTuple):
+class RoundStarted(Message, NamedTuple):
     chat: Chat
     participants: List[User]
     giving_problem: User
 
-class RoundDemandProblem(NamedTuple):
+class RoundDemandProblem(Message, NamedTuple):
     chat: Chat
     user: User
     reply_context: reply.ReplyContext
 
-class RoundAcceptProblem(NamedTuple):
+class RoundAcceptProblem(Message, NamedTuple):
     chat: Chat
 
-class RoundNotifyProblem(NamedTuple):
+class RoundNotifyProblem(Message, NamedTuple):
     chat: Chat
     problem: Problem
 
-class RoundDemandSolutions(NamedTuple):
+class RoundDemandSolutions(Message, NamedTuple):
     chat: Chat
     problem: Problem
     giving_problem: User
     solution_order: List[User]
     reply_context: reply.ReplyContext
 
-class SolutionAlreadySubmitted(NamedTuple):
+class SolutionAlreadySubmitted(Message, NamedTuple):
     chat: Chat
     solution: Solution
 
-class RoundNotFinishedYet(NamedTuple):
+class RoundNotFinishedYet(Message, NamedTuple):
     chat: Chat
     missing_solutions: List[User]
 
-class RoundSummary(NamedTuple):
+class RoundSummary(Message, NamedTuple):
     chat: Chat
     problem: Problem
     solutions: Solution
 
-class RoundRevealed(NamedTuple):
+class RoundRevealed(Message, NamedTuple):
     chat: Chat
     elon_musk: User
