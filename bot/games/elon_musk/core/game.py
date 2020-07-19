1887d5db7ba4e0b72f404aec1759734f69e5a5f4
Matthias Heinzel <mheinzel@posteo.de>
2020-07-19 14:03:33 +0200

    implement join_private

--- bot/games/elon_musk/core/game.py
+++ bot/games/elon_musk/core/game.py
@@ -1,18 +1,26 @@
 import bot.games.elon_musk.core.message as message
+import bot.games.elon_musk.core.reply as reply
+
+def create_game(chat, code):
+    game = Game(chat, code)
+    responses = game.init()
+    return game, responses
 
 class Game:
     def __init__(self, chat, code):
         self.chat = chat
         self.code = code
         self.participants = []
+        self.private_chats = {}
         self.current_round = None
         self.history = []
 
     def init(self):
-        created = message.GameCreated(self.chat, self.code)
-        return [created]
+        return [message.GameCreated(self.chat, self.code)]
 
-def create_game(chat, code):
-    game = Game(chat, code)
-    responses = game.init()
-    return game, responses
+    def join_private(self, user, chat):
+        self.participants += [user]
+        self.private_chats[user.id] = chat
+        res  = [message.GameJoinedPrivate(chat, self.code, user)]
+        res += [message.GameJoined(self.chat, user)]
+        return res
